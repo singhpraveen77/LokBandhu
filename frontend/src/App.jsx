e37dce423eb1054cd6a88efe57aa7dc7249cd247
Raj Maurya <rajmaurya29092004@gmail.com>
2025-09-12 00:30:31 +0530

    test1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 import LokSabha from "./pages/LokSabha";
 // import CreatePostModal from "./pages/CreatePost";
 import Dashboard from "./pages/Dashboard";
-
+//test1
 const router = createBrowserRouter([
   {
     path: "/",
