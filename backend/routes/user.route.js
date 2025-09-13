19464d255a48ab00cfb2f2d882881ee19fee70f8
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-13 12:47:37 +0530

    created user routes controllers and setup axiosinstance and changed folder structure with integrating properly backend and frontend !!

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import signup from "../controllers/user.controller.js";  // adjust path if needed
+import { login, signup } from "../controllers/user.controller.js";  // adjust path if needed
 
 const router = express.Router();
 
 // POST /api/users/signup
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,12 @@
+import { axiosInstance } from "../axios/axiosInstance"
+
+export const login = async (formData) => {
+  try {
+    const res = await axiosInstance.post("/users/login", formData);
+    console.log("✅ Login success:", res.data);
+    return res.data;
+  } catch (error) {
+    console.error("❌ Login error:", error.response?.data || error.message);
+    throw error;
+  }
+};
