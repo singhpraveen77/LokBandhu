067e402f34321487efe1f103a63dc6eecfb2727f
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-15 00:31:51 +0530

    deployment fixed

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api", // ⚡ change for production
-  baseURL: "https://lokbandhu.onrender.com", // ⚡ change for production
+  baseURL: "https://lokbandhu.onrender.com/api", // ⚡ change for production
   headers: {
     "Content-Type": "application/json",
   },
